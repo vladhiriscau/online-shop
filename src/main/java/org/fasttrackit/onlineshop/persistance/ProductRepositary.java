3abdabaf62734597ee28b08d768ed8c5aa1d29ab
vladhiriscau <vlad.hiriscau@yahoo.com>
2020-03-04 21:31:15 +0200

    studied queris derived from method names

--- src/main/java/org/fasttrackit/onlineshop/persistance/ProductRepositary.java
+++ src/main/java/org/fasttrackit/onlineshop/persistance/ProductRepositary.java
@@ -1,7 +1,21 @@
 package org.fasttrackit.onlineshop.persistance;
 
 import org.fasttrackit.onlineshop.domain.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepositary extends JpaRepository<Product, Long> {
+
+    Page<Product> findByNameContaining(String partialName, Pageable pageable);
+
+    Page<Product> findByNameContainingAndQuantityGreaterThan(
+            String partialName, int minQuantity, Pageable pageable);
+
+    //   JPQL syntax
+//    @Query("SELECT product FROM Product product WHERE `name` LIKE '%:partialName%'")
+   //    or using netive queries
+    @Query(value = "select * from product where `name` like '%?0'", nativeQuery = true)
+    Page<Product> findByPartialName(String partialName, Pageable pageable);
 }
